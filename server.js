98fdeec8205776937eb1d43f847870ab44d43e4f
lghiggino <lghiggino@gmail.com>
2020-11-09 15:03:21 -0300

    server and css files

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const http = require("http");
-const fs = require("fs")
+const fs = require("fs");
+const { endianness } = require("os");
 
 const server = http.createServer((req, res) => {
-    //console.log(req.url, req.method);
+    console.log(req.url, req.method);
 
     //set header content type
-    res.setHeader("Content-type", "text/html");
+    res.setHeader("Content-type", "text/html")
 
     //figure out the path the user visited from
     let path = "./views/";
+    //create a switch to respond
     switch(req.url){
-        case("/"):
+        case("/"): 
             path += "index.html"
             break;
         case("/about"):
@@ -18,22 +20,19 @@ const server = http.createServer((req, res) => {
             break;
         default:
             path += "404.html"
-            break;
     }
-
     //read the file
     fs.readFile(path, (err, data) => {
         if (err){
             console.log(err);
             res.end();
-        }
-        //sending the data back to the browser
-        else {
-            res.write(data)
-            //end the response
+            //sending the data back to the browser
+        } else {
+            res.write(data);
             res.end();
-        } 
+        }
     })
+    
 });
 
 server.listen(3000, "localhost", () => {
