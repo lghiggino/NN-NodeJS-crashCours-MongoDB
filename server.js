55ccb92a225dbfdd0c9cc5ee642e44ea3f7eef37
lghiggino <lghiggino@gmail.com>
2020-11-09 10:20:51 -0300

    responses with outside HTML

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
 const http = require("http");
-
-const page = `
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>new File from node</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <!--<link rel='stylesheet' type='text/css' media='screen' href='./css/main.css'>-->
-    <!--<script src='main.js'></script> -->
-</head>
-<body>
-    <section>
-        <h1> This is the response from the server</h1>
-    </section>
-    
-</body>
-</html>
-
-`
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //console.log(req.url, req.method);
 
     //set header content type
     res.setHeader("Content-type", "text/html");
-    //sending the data back to the browser
-    res.write(page);
-    //end the response
-    res.end();
 
+    //figure out the path the user visited from
+    let path = "./views/";
+    switch(req.url){
+        case("/"):
+            path += "index.html"
+            break;
+        case("/about"):
+            path += "about.html"
+            break;
+        default:
+            path += "404.html"
+            break;
+    }
+
+    //read the file
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        }
+        //sending the data back to the browser
+        else {
+            res.write(data)
+            //end the response
+            res.end();
+        } 
+    })
 });
 
 server.listen(3000, "localhost", () => {
