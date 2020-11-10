f3555c0a153b961d215b682f6bf556297a203f0e
lghiggino <lghiggino@gmail.com>
2020-11-10 14:34:53 -0300

    express app init and server init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+//express app
+const app = express();
+
+//listen for requests
+app.listen(3000);
+
+//responding the requests
+app.get("/", (req, res) => {
+    console.log(req.url)
+    //express response send method - auto setHeader and statusCode
+    //res.send("<p> home page </p> <a href='/about'>about</a>")
+    res.sendFile("./views/index.html", {root: __dirname});
+})
+
+app.get("/about", (req, res) => {
+    console.log(req.url)
+    res.sendFile("./views/about.html", {root: __dirname});
+})
+
+//redirects
+app.get("/aboutme", (req, res) => {
+    res.redirect("/about")
+})
+
+//404 page
+app.use((req, res) => {
+    res.sendFile(".views/404.html", {root: __dirname})
+})
