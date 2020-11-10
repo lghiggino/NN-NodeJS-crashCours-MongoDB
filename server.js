370abf22d6d9d16ac978738db4b58d28ab62a2cd
lghiggino <lghiggino@gmail.com>
2020-11-10 08:10:33 -0300

    server redo

--- server.js
+++ server.js
@@ -2,60 +2,47 @@ const http = require("http");
 const fs = require("fs");
 const _ = require("lodash");
 
-
 const server = http.createServer((req, res) => {
-    
-    //lodash
-    const num = _.random(0,20);
-    console.log(num)
-
-    const greet = _.once(() => {
-        console.log("hello");
-    });
-
-    greet()
-    greet()
-
     //set header content type
     res.setHeader("Content-type", "text/html")
 
-    //figure out the path the user visited from
-    let path = "./views/";
+    //figure out the path to the file we will respond with
+    let path = "./views/"
     //create a switch to respond
     switch(req.url){
-        case("/"): 
+        case("/"):
             path += "index.html";
             //set a statusCode
             res.statusCode = 200;
             break;
-        case("/about"):
+        case ("/about"):
             path += "about.html"
             res.statusCode = 200;
             break;
-        //create a redirect
-        case("/about-me"):
+        case ("/aboutme"):
+            //create a redirect
+            res.setHeader("Location", "about")
             res.statusCode = 301;
-            res.setHeader("Location", "/about");
-            res.end();
+            res.end()
             break;
         default:
             path += "404.html"
             res.statusCode = 404;
+            break;
     }
     //read the file
     fs.readFile(path, (err, data) => {
-        if (err){
+        if(err){
             console.log(err);
-            res.end();
+            res.end()
+        }else {
             //sending the data back to the browser
-        } else {
             res.write(data);
-            res.end();
+            res.end()
         }
     })
-});
+})
 
 server.listen(3000, "localhost", () => {
     console.log("listening for requests on port 3000")
 })
-
