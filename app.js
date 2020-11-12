84e093a7a209821838674e96f3934e86b9ca2530
lghiggino <lghiggino@gmail.com>
2020-11-12 07:38:58 -0300

    install EJS and start view engines

--- app.js
+++ app.js
@@ -7,36 +7,37 @@ const app = express();
 app.listen(3000);
 
 //responding the get requests
-app.get( "/", (req, res) => {
-    console.log(req.url);
-     //express response send method - auto setHeader and statusCode
-    //res.send("<p> home page </p> <a href='/about'>about</a>")
-    res.sendFile("./views/index.html", {root: __dirname})
-})
-   
-app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", {root: __dirname});
-})  
+// app.get("/", (req, res) => {
+//     //express response send method - auto setHeader and statusCode
+//     res.sendFile("./views/index.html", {root: __dirname});
+// })
 
-//redirects
-app.get("aboutme", (req, res) => {
-    res.redirect("/about")
-})
+// app.get("/about", (req, res) => {
+//     res.sendFile("./views/about.html", {root: __dirname});
+// })
+//     //sending directhtml - res.send("<p> home page </p> <a href='/about'>about</a>")
+    
+// //redirects
+// app.get("aboutme", (req, res) => {
+//     res.redirect("about")
+// })
 
 //404 page
-app.use((req, res) => {
+app.use( (req, res) => {
     switch(req.url){
         case("/"):
-            res.sendFile("./views/index.html", {root: __dirname})
+            res.sendFile("./views/index.html", {root: __dirname});
             break;
         case("/about"):
             res.sendFile("./views/about.html", {root: __dirname});
             break;
         case("aboutme"):
-            res.redirect("/about")
+            res.redirect("about")
             break;
         default:
             res.status(404).sendFile("./views/404.html", {root: __dirname});
     }
-    //res.status(404).sendFile("./views/404.html", {root: __dirname});
 })
+
+    //res.status(404).sendFile("./views/404.html", {root: __dirname});
+
