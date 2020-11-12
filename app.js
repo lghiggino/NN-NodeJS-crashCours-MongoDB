9953763d3890be653061ee5d1bff3299152cffe7
lghiggino <lghiggino@gmail.com>
2020-11-11 21:27:59 -0300

    server basic redo

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fs = require("fs");
 
 //express app
 const app = express();
@@ -7,32 +6,24 @@ const app = express();
 //listen for requests
 app.listen(3000);
 
-//responding the requests
-// app.get("/", (req, res) => {
-//     console.log(req.url)
-//     //express response send method - auto setHeader and statusCode
-//     //res.send("<p> home page </p> <a href='/about'>about</a>")
-//     res.sendFile("./views/index.html", {root: __dirname});
-// })
+//responding the get requests
+app.get( "/", (req, res) => {
+    console.log(req.url);
+     //express response send method - auto setHeader and statusCode
+    //res.send("<p> home page </p> <a href='/about'>about</a>")
+    res.sendFile("./views/index.html", {root: __dirname})
+})
+   
+app.get("/about", (req, res) => {
+    res.sendFile("./views/about.html", {root: __dirname});
+})  
 
-// app.get("/about", (req, res) => {
-//     console.log(req.url)
-//     res.sendFile("./views/about.html", {root: __dirname});
-// })
-
-// //redirects
-// app.get("/aboutme", (req, res) => {
-//     res.redirect("/about")
-// })
+//redirects
+app.get("aboutme", (req, res) => {
+    res.redirect("/about")
+})
 
 //404 page
 app.use((req, res) => {
-    switch (req.url){
-        case ("/"): res.sendFile("./views/index.html", {root: __dirname});
-        break;
-        case ("/about"): res.sendFile("./views/about.html", {root: __dirname});
-        break;
-        default: res.status(404).sendFile("./views/404.html", {root: __dirname});
-    }
-    //res.sendFile("./views/404.html", {root: __dirname})
+    res.status(404).sendFile("./views/404.html", {root: __dirname});
 })
