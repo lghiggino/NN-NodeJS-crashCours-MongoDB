c6a4bc097a6c75db3e41e5067e5d91f1d0c1ae8c
lghiggino <lghiggino@gmail.com>
2020-11-26 16:36:21 -0300

    review vanilla session

---
+++ review.js
@@ -0,0 +1,53 @@
+const http = require("http")
+const fs = require("fs")
+
+const server = http.createServer( (req, res) => {
+    console.log(req.method, req.url)
+    const path = req.url
+    console.log(path)
+
+    switch (req.url){
+        case("/"):
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html");
+            fs.readFile("./lesson 01/index.html", (error, data) => {
+                if (error) {
+                    console.log(error);
+                    res.end();
+                }else {
+                    res.write(data);
+                    res.end()
+                }
+            });
+            break;
+        case("/blog"):
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html");
+            fs.readFile("./lesson 01/blog.html", (error, data) => {
+                if (error) {
+                    console.log(error);
+                    res.end();
+                }else {
+                    res.write(data);
+                    res.end()
+                }
+            });
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-type", "text/html");
+            fs.readFile("./lesson 01/404.html", (error, data) => {
+                if (error) {
+                    console.log(error);
+                    res.end();
+                }else {
+                    res.write(data);
+                    res.end()
+                }
+            });
+    }
+})
+
+server.listen(3003, "localhost" , () => {
+    console.log("the server is listening");
+})
